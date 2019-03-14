e1d974831d58bac6bc0c6b4ca331e30ab5dc924b
AndrewAndrea <z_huan0619@163.com>
2019-03-15 00:11:51 +0800

    增加评论，搜索，友链

--- andrewblog/blog/views.py
+++ andrewblog/blog/views.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.db.models import Q, F
 
 from django.shortcuts import render
 
@@ -84,3 +84,26 @@ class PostDetailView(CommonViewMixin, DetailView):
     template_name = 'blog/detail.html'
     context_object_name = 'post'
     pk_url_kwarg = 'post_id'
+
+
+class SearchView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context.update({
+            'keyword': self.request.GET.get('keyword', '')
+        })
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        keyword = self.request.GET.get('keyword')
+        if not keyword:
+            return queryset
+        return queryset.filter(Q(title__icontains=keyword) | Q(desc__icontains=keyword))
+
+
+class AuthorView(IndexView):
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        author_id = self.kwargs.get('owner_id')
+        return queryset.filter(owner_id=author_id)
---
+++ andrewblog/comment/templatetags/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# @Time : 2019/3/14 23:55 
+# @Author : Andrew
+# @Site :  
+# @File : __init__.py.py 
+# @Software: PyCharm
